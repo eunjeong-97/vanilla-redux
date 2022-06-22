66343d1e841b69b79934eb4b2dcb060ee1012d12
eunjeong-97 <eoss7777@naver.com>
2022-06-22 16:27:46 +0900

    2-1. state 변경방법

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ const DELETE_TODO = 'DELETE_TODO';
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      // return state.push(action.text) 처럼 직접 수정하면 안된다
+      // 대신 새로운 array를 만들어서 교체해주면 된다
+      // 데이터를 삭제하기 위해 id값을 추가했다
+      return [...state, { text: action.text, id: Date.now() }]
     case DELETE_TODO:
       return [];
     default:
@@ -30,10 +33,9 @@ const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  // createToDo(toDo);
-  // 새로 HTML요소를 만들기보단 dispatch해줄 것이다
-  // 만약 리듀서에게 state의 상태말고도 데이터를 전달하고 싶다면 아래와 같이 추가로 전달하면 된다
-  // 직접적으로 변경하는 mutate state 변수를 사용하면 안된다 
+  // redux 공식문서를 보면 state는 single source of true이고, read-only이기 때문에 직접 변경하면 안된다 store.getcurrent()+1 같이 하면 안된다!!
+  // store를 바꿀 유일한 방법은 dispatch에 action을 보내는 것이다
+  // state를 수정하는 대신 새로운 state를 반환하면 된다
   store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
