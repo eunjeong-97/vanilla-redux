85be215389361f0954668e199c4b94a69b8b8b61
eunjeong-97 <eoss7777@naver.com>
2022-06-23 16:52:52 +0900

    4-1. createAction 액션생성자는 type과 payload만을 가진다

--- src/store.js
+++ src/store.js
@@ -1,21 +1,31 @@
 import { createStore } from 'redux';
-const ADD = 'ADD';
-const DELETE = 'DELETE';
+import { createAction } from '@reduxjs/toolkit';
 
-export const addToDo = text => {
-    return { type: ADD, text };
-}
+// const ADD = 'ADD';
+// const DELETE = 'DELETE';
 
-export const deleteToDo = id => {
-    return { type: DELETE, id };
-}
+// export const addToDo = text => {
+//     return { type: ADD, text };
+// }
+
+// export const deleteToDo = id => {
+//     return { type: DELETE, id };
+// }
+
+const addToDo = createAction('ADD');
+const deleteToDo = createAction('DELETE');
 
 const reducer = (state = [], action) => {
     switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
+        // addToDo나 deleteToDofmf 콘솔로 확인하면 함수지만 .type로 확인하면 텍스트이다
+        // console.log(addToDo(), deleteToDo()) 같이 함수자체를 호출해버리면 액션이 나온다
+        // {type: 'ADD', payload: undefined }, {type:'DELETE', payload: undefined}
+        case addToDo.type: // ADD대신
+            // action도 text속성을 가지는것이 아니라 payload속성값을 가지게 된다
+            // {type:'ADD', payload: 'HELLO'}
+            return [{ text: action.payload, id: Date.now() }, ...state];
+        case deleteToDo.type: // DELETE대신
+            return state.filter(toDo => toDo.id !== action.payload);
         default:
             break;
     }
@@ -27,7 +37,3 @@ store.subscribe();
 export const actionCreators = { addToDo, deleteToDo };
 
 export default store;
-
-// 액션타입, 액션생성자, 리듀서 등과 같은 많은 코드를 사용하는것이 불편해서
-// Redux toolkit을 사용하기도 한다 (지름길이 있는 package)
-// 적은양의 코드로 리덕스 사용가능
